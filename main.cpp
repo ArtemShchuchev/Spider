a2c61f2f2e78175c68f3095f9a7aa566fae626b2
ArtemShchuchev <shuaa@rambler.ru>
2024-01-28 12:36:03 +0700

    добавил подпрограммы

--- main.cpp
+++ main.cpp
@@ -8,10 +8,8 @@
 #include "SecondaryFunction.h"
 #include "Types.h"
 
-static task_t maketaskGetPage(HtmlClient& client, const std::string& urlStr, std::wstring& page) {
-    auto t = [&]() { page = client.getRequest(urlStr); };
-    return t;
-}
+static void spider(LinkList& links);
+static void spiderTask(const Link& url, Thread_pool& tp);
 
 int main(int argc, char** argv)
 {
@@ -32,39 +30,87 @@ int main(int argc, char** argv)
         //const int numThr(7);
         //Thread_pool tp(numThr);
 
-        while (links.empty() == false)
-        {
-            // Загрузка очередной странички
-            Link url(links.front());
-            links.pop_front();
-            std::wstring page;
-            if (url.recLevel > 0) {
-                //std::wcout << L"1. Поиск-чтение страницы...\n";
-                //std::wcout << L"   url: " << utf82wideUtf(url.link_str) << " (" << url.recLevel << ")\n";
-                HtmlClient client;
-                page = client.getRequest(url.link_str); // url -> page
-            }
+        spider(links);
+    }
+    catch (const std::exception& err)
+    {
+        consoleCol(col::br_red);
+        std::wcerr << L"\nИсключение типа: " << typeid(err).name() << '\n';
+        std::wcerr << ansi2wideUtf(err.what()) << '\n';
+        consoleCol(col::cancel);
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+static void spider(LinkList& links)
+{
+    while (links.empty() == false)
+    {
+        // Загрузка очередной странички
+        Link url(links.front());
+        links.pop_front();
+        if (url.recLevel > 0) {
+            //std::wcout << L"1. Поиск-чтение страницы...\n";
+            std::wcout << L"   url: " << utf82wideUtf(url.link_str) << " (" << url.recLevel << ")\n";
+            HtmlClient client;
+            std::wstring page = client.getRequest(url.link_str); // url -> page
 
             // Поиск слов/ссылок на страничке
-            WordMap wordAmount;
             if (page.empty() == false) {
                 //std::wcout << L"2. Парсер слов и ссылок...\n";
                 WordSearch words;
-                std::pair<WordMap, LinkList> wordLink(words.getWordLink(std::move(page), url.recLevel)); // page, recurse -> word, amount, listLink
-                wordAmount = wordLink.first;
-                links.splice(links.end(), wordLink.second);
+                auto [wordAmount, listLinks](words.getWordLink(std::move(page), url.recLevel)); // page, recurse -> word, amount, listLinks
+                links.splice(links.end(), listLinks);
+
+                /*
+                uint32_t listNum(0);
+                for (const auto& link : links) {
+                    std::wcout << ++listNum << ") " << link.link_str << " (" << link.recLevel << ")\n";
+                }
+
+                listNum = 0;
+                for (const auto& [word, amount] : wordAmount) {
+                    std::wcout << ++listNum << ") " << word << " - " << amount << '\n';
+                }
+                */
+
+                /*
+                // Сохранение найденных слов/ссылок в БД
+                if (wordAmount.empty() == false) {
+                    //std::wcout << L"3. Сохраниние в БД...\n";
+                    Clientdb db;
+                    int idLink(db.addLink(url.link_str));
+                    idWordAm_vec idWordAm(db.addWords(std::move(wordAmount)));
+                    db.addLinkWords(idLink, idWordAm);
+                }
+                */
             }
-            /*
-            uint32_t listNum(0);
+        }
+    }
+}
+
+static void spiderTask(const Link& url, Thread_pool& tp)
+{
+    // Загрузка очередной странички
+    if (url.recLevel > 0) {
+        //std::wcout << L"1. Поиск-чтение страницы...\n";
+        //std::wcout << L"   url: " << utf82wideUtf(url.link_str) << " (" << url.recLevel << ")\n";
+        HtmlClient client;
+        std::wstring page = client.getRequest(url.link_str); // url -> page
+
+        // Поиск слов/ссылок на страничке
+        if (page.empty() == false) {
+            //std::wcout << L"2. Парсер слов и ссылок...\n";
+            WordSearch words;
+            auto [wordAmount, links](words.getWordLink(std::move(page), url.recLevel)); // page, recurse -> word, amount, listLink
+
             for (const auto& link : links) {
-                std::wcout << ++listNum << ") " << link.link_str << " (" << link.recLevel << ")\n";
+                tp.add([&link, &tp] {spiderTask(link, tp); });
             }
 
-            listNum = 0;
-            for (const auto& [word, amount] : wordAmount) {
-                std::wcout << ++listNum << ") " << word << " - " << amount << '\n';
-            }
-            */
             /*
             // Сохранение найденных слов/ссылок в БД
             if (wordAmount.empty() == false) {
@@ -77,14 +123,4 @@ int main(int argc, char** argv)
             */
         }
     }
-    catch (const std::exception& err)
-    {
-        consoleCol(col::br_red);
-        std::wcerr << L"\nИсключение типа: " << typeid(err).name() << '\n';
-        std::wcerr << ansi2wideUtf(err.what()) << '\n';
-        consoleCol(col::cancel);
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
 }
