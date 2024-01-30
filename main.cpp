150b9794ac71d62c1b0671224c885ab9f7676fe0
ArtemShchuchev <shuaa@rambler.ru>
2024-01-30 08:48:03 +0700

    nedd tests

--- main.cpp
+++ main.cpp
@@ -14,7 +14,11 @@ struct Lock {
     std::mutex parse;
     std::mutex db;
 };
-static void spiderTask(const Link url, Lock& lock, Thread_pool& threadPool);
+
+Lock* lock;
+Thread_pool* threadPool;
+
+static void spiderTask(const Link url, Lock* lock, Thread_pool* threadPool);
 
 static task_t maketask(Thread_pool& tp, int i)
 {
@@ -37,15 +41,19 @@ int main(int argc, char** argv)
         unsigned int recurse(config.getConfig<unsigned int>("Spider", "recurse"));
         Link url{ std::move(firstLink), recurse };
 
-        Lock lock;
+        //Lock lock;
         int numThr(std::thread::hardware_concurrency());
         
-        Thread_pool threadPool(numThr);
-        threadPool.setTimeout(std::chrono::seconds(5));
+        //Thread_pool threadPool(numThr);
+        lock = new Lock();
+        threadPool = new Thread_pool(numThr);
+        threadPool->setTimeout(std::chrono::seconds(5));
         spiderTask(url, lock, threadPool);
+        //threadPool.add([url, &lock, &threadPool] { spiderTask(url, lock, threadPool); });
         
         //threadPool.add(maketask(threadPool, 5));
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        //std::this_thread::sleep_for(std::chrono::seconds(10));
+        threadPool->wait();
     }
     catch (const std::exception& err)
     {
@@ -60,16 +68,16 @@ int main(int argc, char** argv)
 }
 
 
-static void spiderTask(const Link url, Lock& lock, Thread_pool& threadPool)
+static void spiderTask(const Link url, Lock* lock, Thread_pool* threadPool)
 {
     // Загрузка очередной странички
     if (url.recLevel > 0) {
-        lock.console.lock();
+        lock->console.lock();
         ++countglob;
         int count = countglob;
         //std::wcout << count << L"   url: " << utf82wideUtf(url.link_str) << " (" << url.recLevel << ")\n";
         std::wcout << count << "\n";
-        lock.console.unlock();
+        lock->console.unlock();
         std::wstring page;
         {
             HtmlClient client;
@@ -77,10 +85,10 @@ static void spiderTask(const Link url, Lock& lock, Thread_pool& threadPool)
         }
 
 
-        lock.console.lock();
+        lock->console.lock();
         //std::wcout << count << L" Получена страничка---------\n";
         std::wcout << "\t" << count << "\n";
-        lock.console.unlock();
+        lock->console.unlock();
 
 
 
@@ -90,26 +98,26 @@ static void spiderTask(const Link url, Lock& lock, Thread_pool& threadPool)
             try
             {
                 {
-                    std::lock_guard<std::mutex> lg(lock.parse);
+                    std::lock_guard<std::mutex> lg(lock->parse);
                     WordSearch words;
                     wordlinks = words.getWordLink(std::move(page), url.recLevel); // page, recurse -> word, amount, listLink
                 }
 
 
                 std::this_thread::sleep_for(std::chrono::milliseconds(20));
-                lock.console.lock();
+                lock->console.lock();
                 std::wcout << "\t\t" << count << "\n";
-                lock.console.unlock();
+                lock->console.unlock();
 
 
 
                 for (const auto& link : wordlinks.second) {
-                    threadPool.add([link, &lock, &threadPool] { spiderTask(link, lock, threadPool); });
+                    threadPool->add([link, lock, threadPool] { spiderTask(link, lock, threadPool); });
                 }
             }
             catch (const std::exception& err)
             {
-                std::lock_guard<std::mutex> lg(lock.console);
+                std::lock_guard<std::mutex> lg(lock->console);
                 consoleCol(col::br_red);
                 std::wcerr << L"\nИсключение типа: " << typeid(err).name() << '\n';
                 std::wcerr << L"Ссылка: " << url.link_str << '\n';
@@ -117,17 +125,16 @@ static void spiderTask(const Link url, Lock& lock, Thread_pool& threadPool)
                 consoleCol(col::cancel);
             }
 
-
-            lock.console.lock();
+            lock->console.lock();
             std::wcout << "\t\t\t" << count << "\n";
-            lock.console.unlock();
-
+            lock->console.unlock();
+            
 
             // Сохранение найденных слов/ссылок в БД
             if (wordlinks.first.empty() == false) {
                 try
                 {
-                    std::lock_guard<std::mutex> lg(lock.db);
+                    std::lock_guard<std::mutex> lg(lock->db);
                     Clientdb db;
                     int idLink(db.addLink(url.link_str));
                     idWordAm_vec idWordAm(db.addWords(std::move(wordlinks.first)));
@@ -147,9 +154,8 @@ static void spiderTask(const Link url, Lock& lock, Thread_pool& threadPool)
 
 
 
-            lock.console.lock();
+            std::lock_guard<std::mutex> lg(lock->console);
             std::wcout << "\t\t\t\t" << count << "\n";
-            lock.console.unlock();
         }
     }
 }
